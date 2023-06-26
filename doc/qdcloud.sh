98d9ed47c288ccb1d60838963e4e0caf30cf2982
laoyang103 <553069938@qq.com>
2023-06-26 16:38:32 +0800

    完善部署脚本

--- doc/qdcloud.sh
+++ doc/qdcloud.sh
@@ -12,6 +12,12 @@ echo "192.168.1.66 stu.jxit.net.cn" >> /etc/hosts
 # 增加/etc/rc.local文件的可执行权限，后面开机自启动命令写入此文件
 chmod + /etc/rc.local
 
+# 永久关闭防火墙和selinux
+systemctl stop firewalld
+systemctl disable firewalld
+sed -i "s/enforcing/disabled/g" /etc/sysconfig/selinux
+setenforce 0
+
 read -p "请按任意键继续"
 
 echo "                                                安装控制节点软件                                          "
@@ -77,7 +83,7 @@ mysql -uroot -p123456 jxcms -e "delete from lab_hpv"
 for hpv in ${hpvList[@]}; do
   iplast=$(echo $hpv | awk -F "." '{print $4}')
   hpvname="jxkvm"$iplast
-  mysql -uroot -p123456 jxcms -e "insert into lab_hpv values($iplast, "$hpvname", "$hpv")"
+  mysql -uroot -p123456 jxcms -e "insert into lab_hpv values($iplast, \"$hpvname\", \"$hpv\")"
 done
 
 read -p "请按任意键继续"
@@ -91,6 +97,7 @@ wget http://stu.jxit.net.cn:88/jxfiles/apache-maven-3.6.3.tar.gz
 tar -zxf apache-maven-3.6.3.tar.gz 
 rm apache-maven-3.6.3.tar.gz -rf
 echo "export PATH=\$PATH:/opt/apache-maven-3.6.3/bin/" >> /etc/profile
+source /etc/profile
 
 # 安装tomcat用于运行管理系统
 cd /opt/
@@ -117,9 +124,10 @@ cp target/laboratory_demo.war /opt/tomcat8/webapps/ROOT.war
 /opt/tomcat8/bin/startup.sh
 
 # 因为war包解压出来脚本没有执行权限，所以每分钟执行一次加权限，把脚本目录加入PATH
-echo "export PATH=\$PATH:/opt/tomcat8/webapps/ROOT/WEB-INF/cgi/" >> /etc/profile
 echo "* * * * * chmod +x /opt/tomcat8/webapps/ROOT/WEB-INF/cgi/*" > /tmp/exec.cron
 crontab /tmp/exec.cron
+echo "export PATH=\$PATH:/opt/tomcat8/webapps/ROOT/WEB-INF/cgi/" >> /etc/profile
+source /etc/profile
 
 read -p "请按任意键继续"
 
@@ -166,7 +174,7 @@ systemctl restart dhcpd && systemctl enable dhcpd
 
 # 拷贝TFTP网启程序和启动菜单
 cp tftpboot/* /var/lib/tftpboot/ -r
-systemctl restart tftpd && systemctl enable tftpd
+systemctl restart tftp && systemctl enable tftp
 
 # 拷贝FTP配置文件（匿名访问呢），下载客户端PE镜像
 cp vsftpd.conf /etc/vsftpd/
@@ -185,7 +193,7 @@ allow-update { none; };
 EOF
 
 # 把解析记录拷贝到对应目录，里面的oa dl www都解析到10.16.255.254（管理节点LAN口IP）
-cp doc/conf/named.jxit.net.cn /var/named/
+cp named.jxit.net.cn /var/named/
 chown root:named /var/named/named.jxit.net.cn 
 systemctl restart named && systemctl enable named
 
@@ -201,12 +209,12 @@ iptables -t nat -A POSTROUTING -s 10.64.0.0/16 -j MASQUERADE
 
 read -p "请按任意键继续"
 
-echo "                                    用ansible开始部署计算集群                                             "
+echo "                                    用ansible与计算集群建立免密登录                                             "
 echo "=========================================================================================================="
 
 # 将预先生成的公钥和私钥拷贝到管理节点
-cp /root/laboratory/doc/conf/sshkey/idrsa* ~/.ssh/
-chmod 600 ~/.ssh/idrsa
+cp /root/laboratory/doc/conf/ssh-key/id_rsa* ~/.ssh/
+chmod 600 ~/.ssh/id_rsa*
 # 与所有计算节点建立信任，这里需要输入密码
 for hpv in ${hpvList[@]}; do
   ssh-copy-id root@$hpv
@@ -218,6 +226,15 @@ for hpv in ${hpvList[@]}; do
   echo $hpv >> /etc/ansible/hosts
 done
 
+# 计算节点ssh登录不用DNS验证，加速ansible执行
+ansible jxkvm -m shell -a "sed -i 's/#UseDNS yes/UseDNS no/g' /etc/ssh/sshd_config"
+ansible jxkvm -m shell -a "systemctl restart sshd"
+
+read -p "请按任意键继续"
+
+echo "                                    用ansible安装计算集群软件                                             "
+echo "=========================================================================================================="
+
 # 所有计算节点安装epel软件仓库
 ansible jxkvm -m shell -a "yum -y install epel-release"
 
@@ -226,6 +243,11 @@ ansible jxkvm -m shell -a "yum -y install net-tools psmisc wget unzip vim bc git
 
 # 所有计算节点安装KVM虚拟化软件
 ansible jxkvm -m shell -a "yum -y install qemu-kvm qemu-img libvirt libvirt-python python-virtinst libvirt-client virt-install"
+
+read -p "请按任意键继续"
+
+echo "                                    用ansible配置计算节点KVM网络监听                                      "
+echo "=========================================================================================================="
 
 # 配置libvirtd服务监听网络端口，让管理节点对其进行操控
 ansible jxkvm -m shell -a "sed -i 's/#listen_tls/listen_tls/g' /etc/libvirt/libvirtd.conf"
@@ -241,17 +263,31 @@ ansible jxkvm -m shell -a "systemctl enable libvirtd"
 ansible jxkvm -m shell -a "virsh net-destroy default"
 ansible jxkvm -m shell -a "virsh net-undefine default"
 
+read -p "请按任意键继续"
+
+echo "                                    用ansible配置计算节点关闭防火墙selinux                                "
+echo "=========================================================================================================="
+
 # 所有计算节点关闭防火墙和selinux
 ansible jxkvm -m shell -a "sed -i "s/enforcing/disabled/g" /etc/sysconfig/selinux"
 ansible jxkvm -m shell -a "systemctl stop firewalld; systemctl disable firewalld; setenforce 0"
+
+read -p "请按任意键继续"
+
+echo "                                    用ansible配置计算节点挂载NFS存储目录到虚拟磁盘目录                    "
+echo "=========================================================================================================="
 
 # 所有计算节点挂载存储节点的/data/vdisk到KVM虚拟磁盘目录/var/lib/libvirt/images
 ansible jxkvm -m shell -a 'echo -e "10.16.255.254:/data/vdisk/\t/var/lib/libvirt/images\tnfs\tdefaults\t0\t0" >> /etc/fstab'
 ansible jxkvm -m shell -a "mount -a"
 ansible jxkvm -m shell -a "df | grep images"
 
-# 所有计算节点拷贝网卡和网桥配置样例文件，此处需要自己去每个计算节点配置桥接
-# 配置好后用 systemctl restart network重启生效，远程操作有风险！
+read -p "请按任意键继续"
+
+echo "                   拷贝网卡和网桥配置样例文件到所有计算节点/tmp，此处需要自己去每个计算节点配置桥接       "
+echo "                   配置好后用 systemctl restart network重启生效，远程操作有风险！                         "
+echo "=========================================================================================================="
+
 cd /root/laboratory/
 ansible jxkvm -m copy -a "src=doc/conf/nic/ifcfg-br-vmr dest=/tmp/ifcfg-br-vmr"
 ansible jxkvm -m copy -a "src=doc/conf/nic/ifcfg-enp1s0 dest=/tmp/ifcfg-nic"
