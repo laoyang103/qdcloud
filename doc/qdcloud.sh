fd34a955de92215e163581e5b86c8da330f81f04
laoyang103 <553069938@qq.com>
2023-06-27 17:01:05 +0800

    项目名改为qdcloud

--- doc/qdcloud.sh
+++ doc/qdcloud.sh
@@ -125,7 +125,7 @@ mvn install:install-file -Dfile=lib/rpc-util-1.0.jar -DgroupId=cn.gatherlife -Da
 mvn install:install-file -Dfile=lib/patchca-0.5.0-SNAPSHOT.jar -DgroupId=net.pusuo -DartifactId=patchca -Dversion=0.5.0-SNAPSHOT -Dpackaging=jar
 mvn install:install-file -Dfile=lib/common-object-0.0.1-SNAPSHOT.jar -DgroupId=org.xson -DartifactId=common-object -Dversion=0.0.1-SNAPSHOT -Dpackaging=jar
 mvn package -Dmaven.test.skip=true
-cp target/qdcloud_demo.war /opt/tomcat8/webapps/ROOT.war
+cp target/qdcloud.war /opt/tomcat8/webapps/ROOT.war
 
 # 启动tomcat
 /opt/tomcat8/bin/startup.sh
