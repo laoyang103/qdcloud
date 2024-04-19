ed25a5189457a3125d8af892da80441903359b6c
laoyang103 <553069938@qq.com>
2024-04-19 08:48:55 +0000

    Pod选择网关节点，使用公网DNS

--- src/main/webapp/WEB-INF/cgi/mkenv.sh
+++ src/main/webapp/WEB-INF/cgi/mkenv.sh
@@ -11,15 +11,28 @@ mkdir -p $hpvdiskdir/$user_name/pod
 cp -r yml/template/*.yml $hpvdiskdir/$user_name/
 sed -i "s/__user_vmr/$vmrip/g" $hpvdiskdir/$user_name/*.yml
 sed -i "s/__user_name/$user_name/g" $hpvdiskdir/$user_name/*.yml
+
+echo "### create gateway pod ..."
 kubectl apply -f $hpvdiskdir/$user_name/vpc-subnet.yml
 kubectl apply -f $hpvdiskdir/$user_name/gateway.yml
 kubectl apply -f $hpvdiskdir/$user_name/nat.yml
 
+echo "### get gateway pod node ..."
+gwnode=""
+while true; do
+  gwnode=$($ksys get pod/$gwprefix-$user_name-0 -o wide | grep -v NODE | awk '{print $7}')
+  test -n "$gwnode" && break
+done
+
+$ksys get pod/vpc-nat-gw-gateway-$user_name-0 -o wide
+
+echo "### create yml(not create pod) on $gwnode"
 for vm in ${vmList[@]}; do
   vmname=$(echo $vm | sed "s/jx-//g" | sed "s/^/$user_name-/g")
   ipaddr="10.10.10.$(echo $vm | awk -F "-" '{print $NF}')"
   cp $hpvdiskdir/$user_name/pod.yml $hpvdiskdir/$user_name/pod/$vmname.yml 
   sed -i "s/__vmname/$vmname/g" $hpvdiskdir/$user_name/pod/$vmname.yml
   sed -i "s/__fix_ipaddress/$ipaddr/g" $hpvdiskdir/$user_name/pod/$vmname.yml
-  echo "kubectl apply -f $hpvdiskdir/$user_name/pod/$vmname.yml"
+  sed -i "s/__select_node/$gwnode/g" $hpvdiskdir/$user_name/pod/$vmname.yml
+  echo "create $hpvdiskdir/$user_name/pod/$vmname.yml"
 done
